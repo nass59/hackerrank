bc21196de8463ca56ac5324178dbc1b331266f04
Afrete Nassim <nassim.afrete.ext@francetv.fr>
2018-04-14 20:05:51 +0200

    urlifier

--- data_structure/array_strings/urlifier.php
+++ data_structure/array_strings/urlifier.php
@@ -16,14 +16,20 @@ class Urlifier
     {
         return str_replace(' ', '%20', trim($str));
     }
+}
 
+/**
+ * Class UrlifierWithTrueLength
+ */
+class UrlifierWithTrueLength
+{
     /**
      * @param string $str
      * @param int    $trueLength
      *
      * @return string
      */
-    public static function urlifyWithTrueLength(string $str, int $trueLength): string
+    public static function urlify(string $str, int $trueLength): string
     {
         return str_replace(' ', '%20', substr($str, 0, $trueLength));
     }
@@ -31,5 +37,5 @@ class Urlifier
 
 Urlifier::urlify('Mr John Smith     '); // "Mr%20John%20Smith"
 Urlifier::urlify('    Mr John Smith     '); // "Mr%20John%20Smith"
-Urlifier::urlifyWithTrueLength('Mr John Smith     ', 13); // "Mr%20John%20Smith"
-Urlifier::urlifyWithTrueLength('     Mr John Smith     ', 18); // "%20%20%20%20%20Mr%20John%20Smith"
+UrlifierWithTrueLength::urlify('Mr John Smith     ', 13); // "Mr%20John%20Smith"
+UrlifierWithTrueLength::urlify('     Mr John Smith     ', 18); // "%20%20%20%20%20Mr%20John%20Smith"
